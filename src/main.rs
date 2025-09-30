86c0ee676cd348c601bad9512a8010020c902c1d
NTeditor <nteditor4@gmail.com>
2025-09-30 05:47:44 +0300

    Improve logging, command execution, and Clippy suggestions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#[macro_use]
-mod macros;
 mod error;
 mod termux;
 
@@ -29,6 +27,7 @@ struct Cli {
 fn main() -> Result<(), AppError> {
     init_logger();
     let cli = Cli::parse();
+    log::info!("args parsed");
     let mut command = match cli.command {
         Some(Commands::Exec { args }) => build_user_command(cli.shell, args),
         None => build_command(cli.shell),
@@ -42,7 +41,11 @@ fn main() -> Result<(), AppError> {
         log::error!("failed wait end process: {}", e);
         e
     })?;
-    let exit_code = status.code().unwrap_or(1);
+    let exit_code = status.code().unwrap_or_else(|| {
+        log::warn!("failed get exit code, usege default");
+        1
+    });
+    log::info!("process end. exit code: {}", exit_code);
     std::process::exit(exit_code);
 }
 
@@ -52,36 +55,46 @@ fn init_logger() {
             .with_max_level(log::LevelFilter::Info)
             .with_tag("tsw"),
     );
+    log::info!("logger initialized");
 }
 
 fn build_command(shell_name: String) -> Result<Command, AppError> {
+    log::info!("running interactive shell");
     let termux = Termux::new()?;
 
     let mut command = Command::new(&termux.su_file);
     command.args(["-i", "-c"]);
 
     let env_vars = termux.env.to_string();
+    log::info!("env vars: {}", env_vars);
     let shell = termux.get_shell_file(shell_name)?;
+    log::info!("shell path: {}", shell);
 
-    let root_command = format!("env '-i' {} '{}'", env_vars, shell);
-    command.arg(root_command);
+    let root_command_str = format!("env '-i' {} '{}'", env_vars, shell);
+    log::info!("final root command: {}", root_command_str);
+    command.arg(root_command_str);
     Ok(command)
 }
 
 fn build_user_command(shell_name: String, user_command: Vec<String>) -> Result<Command, AppError> {
+    log::info!("running user command");
     let termux = Termux::new()?;
 
     let mut command = Command::new(&termux.su_file);
     command.args(["-i", "-c"]);
 
     let env_vars = termux.env.to_string();
+    log::info!("env vars: {}", env_vars);
     let user_command_str = user_command.join(" ");
+    log::info!("user command: {}", user_command_str);
     let shell = termux.get_shell_file(shell_name)?;
+    log::info!("shell path: {}", shell);
 
     let root_command_str = format!(
         "env '-i' {} '{}' '-c' '{}'",
         env_vars, shell, user_command_str,
     );
+    log::info!("final root command: {}", root_command_str);
     command.arg(root_command_str);
     Ok(command)
 }
--- src/termux.rs
+++ src/termux.rs
@@ -1,10 +1,11 @@
 use core::fmt;
-use std::{path::PathBuf, vec};
+use std::path::PathBuf;
+use std::process::Command;
 
 use crate::error::AppError;
 
-const TERMUX_FS: &'static str = "/data/data/com.termux/files";
-const TERMUX_TERM: &'static str = "xterm-256color";
+const TERMUX_FS: &str = "/data/data/com.termux/files";
+const TERMUX_TERM: &str = "xterm-256color";
 
 pub struct Termux {
     pub env: TermuxEnv,
@@ -41,10 +42,13 @@ impl Termux {
     }
 
     pub fn get_system_path() -> Result<String, AppError> {
-        let output = sh_output!("su -c 'echo $PATH'").map_err(|e| {
-            log::error!("failed get raw system path: {}", e);
-            e
-        })?;
+        let output = Command::new("su")
+            .args(["-c", "echo $PATH"])
+            .output()
+            .map_err(|e| {
+                log::error!("failed get raw system path: {}", e);
+                e
+            })?;
 
         let stdout_raw = String::from_utf8(output.stdout).map_err(|e| {
             log::error!("failed decode raw system path to String: {}", e);
@@ -64,11 +68,11 @@ pub struct TermuxEnv {
 
 impl fmt::Display for TermuxEnv {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let parts = vec![
-            format!("PATH={}", self.path),
-            format!("HOME={}", self.home),
-            format!("PREFIX={}", self.prefix),
-            format!("TERM={}", self.term),
+        let parts = [
+            format!("'PATH={}'", self.path),
+            format!("'HOME={}'", self.home),
+            format!("'PREFIX={}'", self.prefix),
+            format!("'TERM={}'", self.term),
         ];
 
         write!(f, "{}", parts.join(" "))

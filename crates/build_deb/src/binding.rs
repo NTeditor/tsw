3340d3f87209a72713b4852ecea69bdfe683a7fc
NTeditor <nteditor4@gmail.com>
2025-12-04 20:49:03 +0300

    add custom cargo_subcommand

--- crates/build_deb/src/binding.rs
+++ crates/build_deb/src/binding.rs
@@ -1,6 +1,16 @@
 use anyhow::{Result, bail};
 use std::process::{Command, Stdio};
 
+macro_rules! add_flag {
+    ($name:ident, $flag:expr) => {
+        pub fn $name(&mut self) -> &mut Self {
+            self.arg($flag);
+            self
+        }
+    };
+}
+
+#[derive(Debug)]
 pub struct CargoCmd {
     path: String,
     args: Vec<String>,
@@ -20,13 +30,12 @@ impl CargoCmd {
         self.args.push(value);
     }
 
-    pub fn ndk(&mut self) -> &mut Self {
-        self.arg("ndk");
-        self
-    }
+    add_flag!(build, "build");
+    add_flag!(release, "--release");
 
-    pub fn build(&mut self) -> &mut Self {
-        self.arg("build");
+    pub fn cargo_subcommand<S: Into<String>>(&mut self, subcommand: S) -> &mut Self {
+        let subcommand = subcommand.into();
+        self.arg(subcommand);
         self
     }
 
@@ -34,11 +43,6 @@ impl CargoCmd {
         let target = target.into();
         self.arg("--target");
         self.arg(target);
-        self
-    }
-
-    pub fn release(&mut self) -> &mut Self {
-        self.arg("--release");
         self
     }
 
@@ -52,7 +56,7 @@ impl CargoCmd {
         let child = cmd.spawn()?;
         let output = child.wait_with_output()?;
         if !output.status.success() {
-            bail!("exitcode non-zero");
+            bail!("The cargo process exited with a non-zero code");
         }
         Ok(())
     }
--- crates/build_deb/src/main.rs
+++ crates/build_deb/src/main.rs
@@ -6,26 +6,30 @@ use anyhow::Result;
 use binding::CargoCmd;
 use clap::Parser;
 use config::Config;
+use constcat::concat;
 use deb_rust::{DebFile, binary::DebPackage};
 use std::fs::File;
 use types::Target;
+
+const TERMUX_PREFIX: &str = "/data/data/com.termux/files/usr";
+const TERMUX_TSW: &str = concat!(TERMUX_PREFIX, "/bin/tsw");
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Cli {
     #[arg(short, long)]
     target: Target,
-    #[arg(short, long, default_value = "false")]
+    #[arg(short, long, default_value_t = false)]
     release: bool,
-    #[arg(short, long, default_value = "true")]
-    cargo_ndk: bool,
+    #[arg(short, long)]
+    cargo_subcommand: Option<String>,
 }
 
-fn build(target: &Target, cargo_ndk: bool, release: bool) -> Result<()> {
+fn build(target: &Target, cargo_subcommand: Option<String>, release: bool) -> Result<()> {
     println!("Building");
     let mut binding = CargoCmd::new("cargo");
-    if cargo_ndk {
-        binding.ndk();
+    if let Some(subcommand) = cargo_subcommand {
+        binding.cargo_subcommand(subcommand);
     }
 
     binding.build().target(target.as_ref());
@@ -43,16 +47,17 @@ fn package(target: &Target, release: bool) -> Result<()> {
         let build_type = if release { "release" } else { "debug" };
         format!("target/{}/{}/tsw", target, build_type)
     };
+    let deb_output = {
+        let build_type = if release { "release" } else { "debug" };
+        format!("target/{}/{}/tsw.deb", target, build_type)
+    };
 
     let config = Config::load()?;
     let package = DebPackage::new(&config.name)
         .set_version(&config.version)
         .set_architecture(target.as_deb_arch())
         .set_maintainer(&config.authors.join(", "))
-        .with_file(DebFile::from_path(
-            &source_path,
-            "/data/data/com.termux/files/usr/bin/tsw",
-        )?);
+        .with_file(DebFile::from_path(&source_path, TERMUX_TSW)?);
 
     let package = if let Some(description) = &config.description {
         package.set_description(description)
@@ -60,13 +65,18 @@ fn package(target: &Target, release: bool) -> Result<()> {
         package
     };
 
-    package.build()?.write(File::create("tsw.deb")?)?;
+    package.build()?.write(File::create(deb_output)?)?;
     Ok(())
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
-    build(&cli.target, cli.cargo_ndk, cli.release)?;
-    package(&cli.target, cli.release)?;
+    let Cli {
+        target,
+        release,
+        cargo_subcommand,
+    } = cli;
+    build(&target, cargo_subcommand, cli.release)?;
+    package(&target, release)?;
     Ok(())
 }
